7594128de64b62852ef36ae23f6f5f5229da302c
simonemayers <simone21295@gmail.com>
2021-03-28 15:14:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// names = ['Lisa', 'Kaitlin', 'Jan']
+// function writeCards(names, event = 'surprise'){
+//     for(let i = 0; i<names.length; i++){
+//         console.log("Thank you, ${names}[i], for the wonderful ${event} gift!")
+//     }
+
+// }
+const cards = []
+arr = ["Lisa", "Kaitlin", "Jan"]
+function writeCards(arr, event = surprise){
+    for(let i = 0; i < arr.length; i++) {
+        let sentence = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`; 
+        cards.push(sentence); 
+    }
+    return cards
+};
+function countDown(num){
+    let count = num + 1; 
+    while(num >= 0){
+        console.log(num--);
+    }
+}
